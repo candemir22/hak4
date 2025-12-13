23e0b296b1a6f5039288bd38f2350a890b149147
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 01:11:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const mapElement = document.querySelector('gmp-map');
 async function initMap() {
     const { Map } = (await google.maps.importLibrary('maps'));
-    let markers = [];
-
-}
+    let markers = []; }
 initMap();
 
