ce361e6f3a77acac8eef7ec3b7fba5d7cfdbe195
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 01:19:50 +0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-const mapElement = document.querySelector('gmp-map');
-async function initMap() {
-    const { Map } = (await google.maps.importLibrary('maps'));
-    let markers = []; }
-initMap();
-
